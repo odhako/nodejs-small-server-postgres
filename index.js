b53cb58abc5770426cb6a5091abb2c3f010b6f8c
odhako <odhako@gmail.com>
2023-03-16 15:38:42 +0300

    Add dotenv and add local environment variables

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Application = require('./framework/Application.js');
 const parseJson = require('./framework/parseJson.js');
 const parseUrl = require('./framework/parseUrl.js');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
 
 const app = new Application();
 app.use(parseJson);
-app.use(parseUrl('http://localhost:8080'));
+app.use(parseUrl(`${HOST}:${PORT}`));
 
-app.listen(8080, () => console.log('Server started on PORT 8080'));
+app.listen(8080, () => console.log(`Server started on PORT ${PORT}`));
