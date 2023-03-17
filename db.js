d1945d40e6d7b4ea0bf721d743eac82e6cf192c0
odhako <odhako@gmail.com>
2023-03-17 12:57:14 +0300

    Make database controller pool

---
+++ db.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PSQL_USER = process.env.PSQL_USER;
+const PSQL_PASSWORD = process.env.PSQL_PASSWORD;
+const PSQL_HOST = process.env.PSQL_HOST;
+const PSQL_PORT = process.env.PSQL_PORT;
+const PSQL_DATABASE = process.env.PSQL_DATABASE;
+
+const Pool = require('pg').Pool;
+const pool = new Pool({
+  user: PSQL_USER,
+  password: PSQL_PASSWORD,
+  host: PSQL_HOST,
+  port: PSQL_PORT,
+  database: PSQL_DATABASE,
+
+});
+
+module.exports = pool;
